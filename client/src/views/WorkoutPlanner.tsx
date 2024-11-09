6cb8b4c9ef49e3eb3ed4c712dbf90183011d148e
mleung2019 <50841395+mleung2019@users.noreply.github.com>
2024-11-09 12:13:10 -0800

    Code review for workout planner UI/routing

--- client/src/views/WorkoutPlanner.tsx
+++ client/src/views/WorkoutPlanner.tsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import "./WorkoutPlanner.css";
 
 export const WorkoutPlanner = () => {
+  // Note: "Select Existing Workout Plan" links to /generate-workout.
+  // Perhaps a better path name like /select-workout should be chosen?
+  // We think it may be better to not have two buttons on this page, as then we would need
+  // back buttons. Users may also get confused.
   return (
     <div className="workout-planner-main">
       <h1>Choose a Plan</h1>
