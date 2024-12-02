95fa22f3683261ed0f193f62fd65233dd7b6aea9
mleung2019 <50841395+mleung2019@users.noreply.github.com>
2024-12-01 16:53:44 -0800

    Made documentation consistent with other files in utils

--- client/src/utils/user-utils.ts
+++ client/src/utils/user-utils.ts
@@ -3,16 +3,31 @@ import axios from "axios";
 // Use axios baseURL to define the port
 axios.defaults.baseURL = `http://localhost:${process.env.PORT || 8080}`;
 
+/**
+ * Function to get user data from the server side
+ * @params userId - to retrieve specific user data from the database
+ * @returns The username, email, goals, and streak count
+ */
 export async function fetchUserData(userId: string): Promise<any> {
   const response = await axios.get(`/users/${userId}`);
   return response.data;
 }
 
+/**
+ * Function to increment streak data from the server side
+ * @params userId - to increment streak for specific user from the database
+ * @returns The streak count
+ */
 export async function incrementStreak(userId: string): Promise<any> {
   const response = await axios.patch(`/streaks/${userId}/increment`);
   return response.data;
 }
 
+/**
+ * Function to reset streak data from the server side
+ * @params userId - to reset streak for specific user from the database
+ * @returns The streak count (0)
+ */
 export async function resetStreak(userId: string): Promise<any> {
   const response = await axios.patch(`/streaks/${userId}/reset`);
   return response.data;
